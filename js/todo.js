e5557e2d6bbb0297d98ef40e57b1a32903d709e1
sochetanie <allagorik2901@gmail.com>
2017-06-09 14:43:54 -0400

    done

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  submitForm()
 });
 
 // define functions here
+
+function submitForm() {
+  $('form').on('submit', function(event){
+    var input = $('#item').val();
+    $("ol").append(`<li>${input}</li>`);
+    event.preventDefault();
+  });
+}
